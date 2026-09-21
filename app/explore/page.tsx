2b7fb24d9392177a048dcb48396dfeda7d80c8ac
arfuu0-spec <arfuu0@gmail.com>
2026-09-21 23:35:58 +0530

    Update page.tsx

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { supabase } from '../lib/supabase';
+import { supabase } from '../../lib/supabase';
 
 export default function ExplorePage() {
   const [posts, setPosts] = useState<any[]>([]);
